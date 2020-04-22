6e66d289444bc68966261d1a2b540315865d4e59
Bikash Ranjan Ojha <bikashojha101@gmail.com>
2020-04-22 19:35:35 +0530

    assingment 2 and form control

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import DishDetail from './DishdetailComponent';
-import Contact from './ContactComponent'
-import Header from './HeaderComponent'
-import Footer from './FooterComponent'
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comment';
 import { PROMOTIONS } from '../shared/promotions';
@@ -46,6 +47,7 @@ class Main extends Component{
             <Route path="/home" component={HomePage}/>
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
             <Route path="/menu/:dishId" component={DishWithId}/>
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />}/>
             <Route exact path="/contactus" component={Contact} />
             <Redirect to="/home"/>
           </Switch>
