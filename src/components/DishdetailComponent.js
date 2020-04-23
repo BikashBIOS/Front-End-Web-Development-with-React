9d992a749cc1ac0c37accc9bd7113e11652e0362
Bikash Ranjan Ojha <bikashojha101@gmail.com>
2020-04-23 23:52:01 +0530

    animations and its components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import 'font-awesome/css/font-awesome.min.css';
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 const required = val => val && val.length;
@@ -16,6 +17,7 @@ function RenderDish({dish}) {
   
   return (
       <div className="col-12 col-md-5 m-1">
+        <FadeTransform in transformProps={{ exitTransform: 'scale(0.5) translateY(-50%)' }}>
         <Card>
           <CardImg top src={baseUrl + dish.image} alt={dish.name} />
           <CardBody>
@@ -23,6 +25,7 @@ function RenderDish({dish}) {
             <CardText>{dish.description}</CardText>
           </CardBody>
         </Card>
+        </FadeTransform>
       </div>
       
   );
@@ -33,15 +36,20 @@ function RenderComments({comments, postComment, dishId}){
     return(
       <div className="col-12 col-md-5 m-1">
         <h4>Comments</h4>
-        <ul className="list-unstyled">{comments.map((comment)=>{
+        <ul className="list-unstyled">
+          <Stagger in>
+          {comments.map((comment)=>{
           return(
+            <Fade in>
             <li key={comment.id}>
               <p>{comment.comment}</p>
               <p>-- {comment.author}, {new Intl.DateTimeFormat("en-US", {year: "numeric",month: "short",day: "2-digit"}).format(
                 new Date(Date.parse(comment.date)))}</p>
             </li>
+            </Fade>
           );
           })}
+          </Stagger>
         </ul>
         <CommentForm dishId={dishId} postComment={postComment}/>
       </div>
