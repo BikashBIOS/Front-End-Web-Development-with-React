f4a8f3fb6316e62e0358bc793efb58a5193ba8e6
Bikash Ranjan Ojha <bikashojha101@gmail.com>
2020-04-23 17:30:43 +0530

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -9,6 +9,8 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import {Link} from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from './LoadingComponent';
+
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -171,7 +173,25 @@ class CommentForm extends Component {
 }
 
 const DishDetail = (props) => {
-  if(props.dish!=null){
+  if (props.isLoading) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <Loading />
+            </div>
+        </div>
+    );
+  }
+  else if (props.errMess) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <h4>{props.errMess}</h4>
+            </div>
+        </div>
+    );
+  }
+  else if (props.dish != null) {
     return(
       <div class="container">
         <div className="row">
